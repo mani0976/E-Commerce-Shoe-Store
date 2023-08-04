277adb9e82eb4b29006568541e280b6f62156d42
mani0976 <140725712+mani0976@users.noreply.github.com>
2023-08-05 03:32:02 +0530

    Add files via upload

---
+++ document/Data.js
@@ -0,0 +1,99 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shoes",
+    price: 45,
+    desc: "Post game, errands and more—adjust your comfort on the go. The adjustable, padded strap lets you perfect your fit, while the lightweight foam midsole brings first-class comfort to your feet.",
+    img: "images/1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Puma Running Shoes",
+    price: 75,
+    desc: "These U.S. Polo Assn. loafers will make a sound addition to your AM-to-PM footwear capsule. A round toe and panelled construction add to its design. The cushioned soles and a breathable build make them a reliable piece to carry you wherever you go..",
+    img: "images/21.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Brown Sneakers",
+    price: 55,
+    desc: "The radiance lives on in the Nike Air Force 1 '07, the basketball original that puts a fresh spin on what you know best: durably stitched overlays,  clean finishes and the perfect amount of flash to make you shine.",
+    img: "images/11.jpg",
+  },
+  {
+    id: "anilsai",
+    name: "Black Shoes",
+    price: 85,
+    desc: "Bridge the gap between your weekend training run and race day in a durable design that can be deployed not just at the starting line of your favourite race but in the days and months after your conquest.",
+    img: "images/22.jpg",
+  },
+  {
+    id: "manikanta",
+    name: "Loafers",
+    price: 95,
+    desc: "It offers comfort and reliability but with a propulsive sensation that'll help you feel fast and fresh. That kind of versatility is uncommon in the running arena. But who said you can't have it all?",
+    img: "images/13.jpg",
+  },
+  {
+    id: "rajesh",
+    name: "Puma Sneakers ",
+    price: 70,
+    desc: "The radiance lives on in the Nike Air Force 1 '07, the basketball orign  that puts a fresh spin on what you know best: durably stitched over  clean finishes and the perfect amount of flash to make you shine.",
+    img: "images/23.jpg",
+  },
+  {
+    id: "manasa",
+    name: "HRx Black Sneakers ",
+    price: 52,
+    desc: "  With maximum cushioning to support every mile,  the Invincible 3 gives you our highest level of comfort underfoot to help you stay on your feet  today, tomorrow and beyond.",
+    img: "images/24.jpg",
+  },
+  {
+    id: "tejaswi",
+    name: "Nike Running Shoes ",
+    price: 100,
+    desc: "Inspired by the original AJ1, this mid-top edition maintains  the iconic look you love while choice colours and crisp leather  give it a distinct identity.",
+    img: "images/20.jpg",
+  },
+
+  {
+    id: "sai",
+    name: "Nike Black Running Shoes ",
+    price: 90,
+    desc: "he radiance lives on in the Nike Air Force 1 '07, the basketball orign  that puts a fresh spin on what you know best:",
+    img: "images/27.jpg",
+  },
+
+  {
+    id: "balu",
+    name: "White Sneakers",
+    price: 60,
+    desc: "Inspired by the original AJ1, this mid-top edition maintains  the iconic look you love while choice colours",
+    img: "images/28.jpg",
+  },
+
+  {
+    id: "srinu",
+    name: "Spark Black Shoes",
+    price: 80,
+    desc: "With maximum cushioning to support every mile,  the Invincible 3 gives you our highest level of comfort underfoot",
+    img: "images/29.jpg",
+  },
+
+  {
+    id: "shiva",
+    name: "running shoes ",
+    price: 70,
+    desc: "These U.S. Polo Assn. loafers will make a sound addition to your AM-to-PM footwear capsule. A round toe and panelled c",
+    img: "images/30.jpg",
+  },
+
+  {
+    id: "gopi",
+    name: "Mens Suit",
+    price: 45,
+    desc: "Post game, errands and more—adjust your comfort on the go. The adjustable, padded strap lets you perfect your fit",
+    img: "images/31.jpg",
+  },
+
+];
